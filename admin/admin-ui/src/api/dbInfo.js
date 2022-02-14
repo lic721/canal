b98a3c8a53fb29f84bb75bd1fa7dd6aaeb3f5ef8
zhihua.li <zhihua.li@ly.com>
2022-02-14 18:13:54 +0800

    数据库信息管理

---
+++ admin/admin-ui/src/api/dbInfo.js
@@ -0,0 +1,39 @@
+import request from '@/utils/request'
+
+export function getDbInfos(params) {
+  return request({
+    url: '/dbInfos',
+    method: 'get',
+    params: params
+  })
+}
+
+export function addDbInfo(data) {
+  return request({
+    url: '/dbInfo',
+    method: 'post',
+    data
+  })
+}
+
+export function dbInfoDetail(id) {
+  return request({
+    url: '/dbInfo/' + id,
+    method: 'get'
+  })
+}
+
+export function updateDbInfo(data) {
+  return request({
+    url: '/dbInfo',
+    method: 'put',
+    data
+  })
+}
+
+export function deleteDbInfo(id) {
+  return request({
+    url: '/dbInfo/' + id,
+    method: 'delete'
+  })
+}
